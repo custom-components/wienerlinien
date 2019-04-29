2cb31e4ec1a825a7d7008297953d7fb6ece89467
Joakim Sørensen <joasoe@gmail.com>
2019-04-29 14:48:30 +0200

    Update sensor.py

--- custom_components/wienerlinien/sensor.py
+++ custom_components/wienerlinien/sensor.py
@@ -12,7 +12,7 @@ from homeassistant.helpers.entity import Entity
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 
-__version__ = '1.1.2'
+__version__ = '1.2.0'
 
 CONF_STOPS = 'stops'
 CONF_APIKEY = 'apikey'
